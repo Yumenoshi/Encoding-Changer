2f8c551891556997a9cee3d442e337e697b224b9
Yumenoshi <Yumenoshi@users.noreply.github.com>
2019-02-19 15:59:13 +0100

    First implementation
    Simply changes  file encoding from system default to unicode.

--- Encoding-Changer/Program.cs
+++ Encoding-Changer/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Encoding_Changer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] arguments)
         {
+            if (arguments.Length >0)
+            {
+                String filePath = arguments[0];
+                String filePathTemp = filePath+"temp";
+                if (File.Exists(filePath))
+                {
+                    String[] fileContent = File.ReadAllLines(filePath,Encoding.Default);
+                    File.WriteAllLines(filePathTemp, fileContent, Encoding.Unicode);
+
+                    File.Delete(filePath);
+                    File.Move(filePathTemp, filePath);
+
+                }
+
+
+
+                Environment.Exit(0);
+            }
         }
     }
 }
